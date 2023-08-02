bcbd0a4baa9f450e43d28818c2b2f251e9bf649a
nankiee <trannhatnam912@gmail.com>
2023-08-02 21:23:55 +0700

    Menu

---
+++ Game3D/Assets/Script/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Game3D/Assets/Script/SplashMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TakeToMenu());
+    }
+
+    IEnumerator TakeToMenu()
+    {
+        yield return new WaitForSeconds(7);
+        SceneManager.LoadScene(1);
+    }
+}
