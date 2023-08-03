705c886b458e2ed093c05f6ef7702391d0842d70
Tuo238 <billbui3002@gmail.com>
2023-08-03 16:52:26 +0700

    added laptop

--- Game3D/Assets/Script/CheckPassword.cs
+++ Game3D/Assets/Script/CheckPassword.cs
@@ -3,41 +3,61 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-
 public class CheckPassword : MonoBehaviour
 {
     private InputField inputTextField;
+    public GameObject doorLocked;
+    public GameObject doorUnlocked;
+    public GameObject display;
+    public GameObject trigger;
 
-
+    public AudioClip correctSoundClip; // Assign the correct sound clip to play in the Inspector
+    public AudioClip wrongSoundClip;   // Assign the wrong sound clip to play in the Inspector
+    private AudioSource audioSource;
 
     void Start()
     {
         inputTextField = GetComponent<InputField>();
-        //text = GetComponent<Text>();
+        audioSource = GetComponent<AudioSource>();
         Cursor.visible = true;
         Screen.lockCursor = false;
-
-
     }
 
     public void CheckPasswordAndLoadScene()
     {
         switch (inputTextField.text)
         {
-            case "qwerty":
-                Debug.Log("hello");
+            case "werty":
+                RightAnswer();
                 break;
             case "12345":
                 Debug.Log("hello2");
                 break;
 
             default:
-                inputTextField.text = "";
+                WrongAnswer();
                 break;
         }
     }
 
-    public void UpdateText()
+    public void RightAnswer()
     {
+        doorLocked.SetActive(false);
+        doorUnlocked.SetActive(true);
+        display.SetActive(false);
+        trigger.SetActive(false);
+        Cursor.visible = false;
+        Screen.lockCursor = true;
+
+        // Play the correct sound
+        audioSource.clip = correctSoundClip;
+        audioSource.Play();
+    }
+
+    public void WrongAnswer()
+    {
+        // Play the wrong sound
+        audioSource.clip = wrongSoundClip;
+        audioSource.Play();
     }
 }
